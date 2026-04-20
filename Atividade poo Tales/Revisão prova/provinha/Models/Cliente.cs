df48fceb44eaa24c95f7fd60ef2bb377655b181d
Mayk-Gabriel <mayk351gabriel@gmail.com>
2026-04-20 12:45:59 -0300

    feat/mayk: revisaõ da prova dj

---
+++ Atividade poo Tales/Revisão prova/provinha/Models/Cliente.cs
@@ -0,0 +1,11 @@
+namespace provinha.Models;
+
+public class Cliente
+{
+    public Cliente(string nome)
+    {
+        Nome = nome;
+    }
+    
+    public string Nome { get; }
+}
---
+++ Atividade poo Tales/Revisão prova/provinha/Program.cs
@@ -0,0 +1,11 @@
+﻿using provinha.Models;
+
+Mesa mesa = new Mesa(11);
+Cliente cliente = new Cliente("Josefino");
+
+DateTime data = DateTime.Now;
+Reserva reserva = new Reserva(cliente, 6, mesa, data);
+mesa.CancelarReserva(data);
+Reserva reserva2 = new Reserva(cliente, 6, mesa, data);
+
+mesa.ExibirReservas();
